dc005bb8b0eeed2e5cf227d0d68bbec64d41c613
Victor Barreau <victor.barreau@zephalto.com>
2025-04-28 17:52:56 +0200

    class param

--- 02_RANS/maillage.py
+++ 02_RANS/maillage.py
@@ -21,6 +21,13 @@ def get_nodes_from_faces(faces_index:list)->list:
                 nodes.append(faces_index[i].nodes[j])
     return nodes
 
+class Parametres():
+    """Classe contenant les paramètres physiques d'un point"""
+    def __init__(self, T:float = 300, p:float = 1e5,V:np.array=np.array([0,0])):
+        self.T = T
+        self.p = p
+        self.v = V
+
 
 class Mesh():
 
@@ -29,6 +36,9 @@ class Mesh():
         self.nodes = self.mesh_read_nodes(filename)
         self.faces = self.mesh_read_faces(filename, self.nodes)
         self.cells = self.mesh_read_cells(filename, self.faces, self.nodes)
+        for face in self.faces:
+            face.set_owner(self.nodes, self.cells)
+            face.set_mean_param(self.cells)
 
     def  mesh_read_nodes(self,filename:str)->np.array:
         """Lecture du maillage à partir d'un fichier"""
@@ -51,12 +61,14 @@ class Mesh():
             line = lines[i]
             if line[0]=='f':
                 index = len(faces)
+                if index == 3 :
+                    a=1
                 # On récupère les indices des noeuds de la face
                 nodes_index = [int(x) for x in line[1:].split()]
                 cells = []
                 for j in range(i,len(lines)):
                     l = lines[j]
-                    if l[0]=='c' and f" {index} " in l:
+                    if l[0]=='c' and f" {index}" in l:
                         # On récupère les indices des cellules de la face
                         cells.append(int(l[1]))
                 # On crée la face
@@ -92,12 +104,12 @@ class Mesh():
         return cells
 
     class Face():
-        def __init__(self, i:int, nodes_index:list, cells,nodes):
+        def __init__(self, i:int, nodes_index:list, cells_index,nodes):
             self.indice_global = i
             self.nodes_index = np.array(nodes_index)
             self.centroid = centre(nodes[nodes_index])
             self.surface = self.length(nodes) * self.get_normal(nodes)
-            self.cells = np.array(cells)
+            self.cells = np.array(cells_index)
 
 
         def __repr__(self):
@@ -121,6 +133,7 @@ class Mesh():
         def set_owner(self,nodes:np.array,cells)->None:
             """Set le propriétaire de la face"""
             self.owner = -1
+            self.neighbour = self.cells[0]
             vec0 = self.centroid + self.get_normal(nodes)
             vec1 = self.centroid - self.get_normal(nodes)
             for i in range(len(self.cells)):
@@ -130,7 +143,21 @@ class Mesh():
                 d2 = np.linalg.norm(vec1 - c_centre)
                 if d1 > d2:
                     self.owner = c.indice_global
+                    if c.indice_global != self.cells[i]:
+                        self.neighbour = c.indice_global
+                    elif len(self.cells) > 1:
+                        self.neighbour = self.cells[1-i]
                     break
+        
+        def set_mean_param(self,cells) : 
+            Vo = cells[self.owner].volume
+            Vn = cells[self.neighbour].volume
+            gn = Vo/(Vo + Vn)
+            go = 1-gn
+            T = gn*cells[self.neighbour].param.T + go*cells[self.owner].param.T
+            v = gn*cells[self.neighbour].param.v + go*cells[self.owner].param.v
+            p = gn*cells[self.neighbour].param.p + go*cells[self.owner].param.p
+            self.param = Parametres(T,p,v)
 
 
 
@@ -146,9 +173,7 @@ class Mesh():
             self.volume = self.get_volume(nodes)
             self.centroid = np.mean(nodes[self.nodes], axis=0)
             self.sort_nodes(nodes)
-            self.T = 0
-            self.v = np.array([0,0])
-            self.p = 0
+            self.param = Parametres()
 
 
         def __repr__(self):
@@ -196,7 +221,32 @@ class Mesh():
                 
             return grad / len(self.nodes)
 
-
+    def compute_gradient(self, var:str)->np.array:
+        """Calcul du gradient d'une variable dans la cellule"""
+        # On calcule le gradient de la variable dans la cellule
+        # On utilise la méthode des différences finies
+        grad = np.zeros(len(self.cells),2)
+        for i in range(len(self.faces)):
+            f = self.faces[i]
+            flux_f = f.get_flux(var)*f.surface
+            if f.owner != -1:
+                grad[f.owner] += flux_f
+                grad[f.neighbour] -= flux_f
+            else:
+                grad[f.neighbour] -= flux_f
+        grad /= self.volume
+                
+        return grad / len(self.nodes)
+    
+    def plot_mesh(self)->None:
+        """Affichage du maillage"""
+        plt.figure()
+        for i in range(len(self.faces)):
+            f = self.faces[i]
+            nodes = self.nodes[f.nodes_index]
+            plt.plot(nodes[:,0], nodes[:,1], 'k-')
+            plt.fill(nodes[:,0], nodes[:,1], alpha=0.2)
+        plt.show()
      
 
 if __name__ ==  "__main__" :
@@ -204,8 +254,7 @@ if __name__ ==  "__main__" :
     cell = mesh.cells[0]
     print( "Volume cellule : " , cell.volume)  # Affiche la surface du triangle
     print("centre de la cellule : ",cell.centroid)  # Affiche le centre de gravité du triangle
-    for face in mesh.faces:
-        face.set_owner(mesh.nodes,mesh.cells)
+    
     face1 = mesh.faces[0]
     print(face1.__repr__())  
     print(f"normale 1 : {face1.get_normal(mesh.nodes)}")
@@ -219,3 +268,5 @@ if __name__ ==  "__main__" :
     cell.v = np.array([1,0])
     cell.p = 101325
     print(cell.__repr__())  # Affiche la cellule
+
+    mesh.plot_mesh()  # Affiche le maillage
